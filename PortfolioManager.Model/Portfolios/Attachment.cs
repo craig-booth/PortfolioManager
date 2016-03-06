4106e5eb656b811c1e51b717f09bf78b751d163f
Craig Booth <craigb@neller.com.au>
2016-03-06 10:52:57 +1030

    Create AttachmentService

---
+++ PortfolioManager.Model/Portfolios/Attachment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using PortfolioManager.Model.Data;
+
+namespace PortfolioManager.Model.Portfolios
+{
+    public class Attachment : IEntity
+    {
+        public Guid Id { get; private set; }
+        public Stream Data { get; private set; }
+
+        public Attachment()
+            :this(Guid.NewGuid())
+        {
+            
+        }
+
+        public Attachment(Guid id)
+        {
+            Id = id;
+
+            Data = new MemoryStream();
+        }
+
+    }
+}
---
+++ PortfolioManager.Service/AttachmentService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using PortfolioManager.Model.Portfolios;
+using PortfolioManager.Model.Data;
+
+namespace PortfolioManager.Service
+{
+    class AttachmentService
+    {
+        private Dictionary<Guid, Attachment> _Attachments;
+
+        internal AttachmentService()
+        {
+            _Attachments = new Dictionary<Guid, Attachment>();
+        }
+
+        public Attachment CreateAttachment(Stream stream)
+        {
+            var attachment = new Attachment();
+
+            stream.CopyTo(attachment.Data);
+            
+            return attachment;
+        }
+
+        public void DeleteAttachment(Guid id)
+        {
+            _Attachments.Remove(id);
+        }
+
+        public Attachment GetAttachment(Guid id)
+        {
+            if (_Attachments.ContainsKey(id))
+                return _Attachments[id];
+            else
+                throw new RecordNotFoundException(id);
+        }
+    }
+}
