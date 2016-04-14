bd0610a7d648c05fa8698c1745f1b68d1f839a06
Craig Booth <craigb@neller.com.au>
2016-04-14 20:09:02 +0930

    change layout of portfolio summary view

--- PortfolioManager.UI/ViewModels/PortfolioSummaryViewModel.cs
+++ PortfolioManager.UI/ViewModels/PortfolioSummaryViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Drawing;
+using System.Globalization;
 
 using PortfolioManager.Service;
 
@@ -38,6 +41,7 @@ namespace PortfolioManager.UI.ViewModels
     class HoldingItemViewModel
     {
         public string ASXCode { get; private set; }
+        public string CompanyName { get; private set; }
         public decimal CurrentValue { get; private set;  }
         public decimal CapitalGain { get;  private set; }
         public decimal CapitalGainPercentage { get; private set; }
@@ -47,6 +51,7 @@ namespace PortfolioManager.UI.ViewModels
         public HoldingItemViewModel(ShareHolding holding)
         {
             ASXCode = holding.Stock.ASXCode;
+            CompanyName = holding.Stock.Name;
             CurrentValue = holding.MarketValue;
             CapitalGain = holding.MarketValue - holding.TotalCostBase;
             if (holding.TotalCostBase != 0)
@@ -58,5 +63,35 @@ namespace PortfolioManager.UI.ViewModels
             HoldingSummaryView = new ViewWithData("HoldingSummary", holding);
         }
 
+    }
+
+    [ValueConversion(typeof(decimal), typeof(Brush))]
+    class DecimalToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((decimal)value >= 0)
+                return Brushes.Green;
+            else
+                return Brushes.Red;
+
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    [ValueConversion(typeof(decimal), typeof(bool))]
+    class AmountIncreased : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((decimal)value >= 0);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
     }
 }
