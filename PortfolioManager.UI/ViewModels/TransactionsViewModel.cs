03285f1eb2d640f6e99c666cb19f172b6271714d
Craig Booth <craigb@neller.com.au>
2016-08-14 21:13:44 +0930

    Create transaction view models for each transaction type

--- PortfolioManager.UI/ViewModels/TransactionsViewModel.cs
+++ PortfolioManager.UI/ViewModels/TransactionsViewModel.cs
@@ -19,6 +19,7 @@ namespace PortfolioManager.UI.ViewModels
     {
         private IStockParameter _StockParameter;
         private IDateRangeParameter _DateParameter;
+        private TransactionViewModelFactory _TransactionViewModelFactory;
 
         public void ParameterChange(object sender, PropertyChangedEventArgs e)
         {
@@ -39,24 +40,22 @@ namespace PortfolioManager.UI.ViewModels
             }
         }
 
-        public ObservableCollection<TransactionViewItem> Transactions { get; private set; }
+        public ObservableCollection<TransactionViewModel> Transactions { get; private set; }
 
-        private TransactionViewItem _SelectedTransactionViewItem;
-        public TransactionViewItem SelectedTransactionViewItem
+        private TransactionViewModel _SelectedTransactionViewModel;
+        public TransactionViewModel SelectedTransactionViewModel
         {
             get
             {
-                return _SelectedTransactionViewItem;
+                return _SelectedTransactionViewModel;
             }
             set
             {
-                _SelectedTransactionViewItem = value;
+                _SelectedTransactionViewModel = value;
                 
                 OnPropertyChanged();
             }
         }
-
-        public TransactionViewModel SelectedTransaction { get; private set; }        
 
         public List<string> TransactionTypes { get; private set; }
 
@@ -69,8 +68,9 @@ namespace PortfolioManager.UI.ViewModels
             _Heading = label;
             _StockParameter = stockParameter;
             _DateParameter = dateParameter;
+            _TransactionViewModelFactory = new ViewModels.TransactionViewModelFactory(Portfolio.StockService);
 
-            Transactions = new ObservableCollection<TransactionViewItem>();
+            Transactions = new ObservableCollection<TransactionViewModel>();
             TransactionTypes = new List<string>();
 
             TransactionTypes.Add("Aquisition");
@@ -118,7 +118,7 @@ namespace PortfolioManager.UI.ViewModels
                 if (transaction.Type != TransactionType.CashTransaction)
                 {
                     var stock = Portfolio.StockService.Get(transaction.ASXCode, transaction.RecordDate);
-                    Transactions.Add(new TransactionViewItem(stock, transaction));
+                    Transactions.Add(_TransactionViewModelFactory.CreateTransactionViewModel(transaction));
                 }
             }
 
@@ -128,40 +128,66 @@ namespace PortfolioManager.UI.ViewModels
 
     }
 
-    class TransactionViewItem
-    {
-        public Transaction Transaction { get; private set; } 
-        public string ASXCode { get; private set; }
-        public string CompanyName { get; private set; }
-
-        public DateTime TransactionDate { get; private set; }
-        public string Description { get; private set; }
-
-        public TransactionViewItem(Stock stock, Transaction transaction)
-        {
-            Transaction = transaction;
-            ASXCode = stock.ASXCode;
-            CompanyName = string.Format("{0} ({1})", stock.Name, stock.ASXCode);
-
-            TransactionDate = transaction.TransactionDate;
-
-            Description = transaction.Description;
-        }
-    }
-
     abstract class TransactionViewModel
     {
+        private StockService _StockService;
+        private Stock _Stock;
+
+        public Transaction Transaction { get; private set; }
+        public string CompanyName { get; private set; }
+        public string Description { get; private set; }
+
         public DateTime TransactionDate { get; set; }
         public string ASXCode { get; set; }
         public DateTime RecordDate { get; set; }
         public string Comment { get; set; }
 
-        public TransactionViewModel(Transaction transaction)
+        public TransactionViewModel(Transaction transaction, StockService stockService)
         {
+            _StockService = stockService;
+
+            Transaction = transaction;
+
+            Description = transaction.Description;
             TransactionDate = transaction.TransactionDate;
             ASXCode = transaction.ASXCode;
             RecordDate = transaction.RecordDate;
             Comment = transaction.Comment;
+
+            _Stock = _StockService.Get(ASXCode, RecordDate);
+            CompanyName = string.Format("{0} ({1})", _Stock.Name, ASXCode);
+        }
+    }
+
+    class TransactionViewModelFactory
+    {
+        private StockService _StockService;
+
+        public TransactionViewModelFactory(StockService stockService)
+        {
+            _StockService = stockService;
+        }
+
+        public TransactionViewModel CreateTransactionViewModel(Transaction transaction)
+        {
+            if (transaction is Aquisition)
+                return new AquisitionViewModel(transaction as Aquisition, _StockService);
+            else if (transaction is CashTransaction)
+                return new CashTransactionViewModel(transaction as CashTransaction, _StockService);
+            else if (transaction is CostBaseAdjustment)
+                return new CostBaseAdjustmentViewModel(transaction as CostBaseAdjustment, _StockService);
+            else if (transaction is Disposal)
+                return new DisposalViewModel(transaction as Disposal, _StockService);
+            else if (transaction is IncomeReceived)
+                return new IncomeReceivedViewModel(transaction as IncomeReceived, _StockService);
+            else if (transaction is OpeningBalance)
+                return new OpeningBalanceViewModel(transaction as OpeningBalance, _StockService);
+            else if (transaction is ReturnOfCapital)
+                return new ReturnOfCapitalViewModel(transaction as ReturnOfCapital, _StockService);
+            else if (transaction is UnitCountAdjustment)
+                return new UnitCountAdjustmentViewModel(transaction as UnitCountAdjustment, _StockService);
+            else
+                throw new NotSupportedException();
         }
     }
 
@@ -172,13 +198,76 @@ namespace PortfolioManager.UI.ViewModels
         public decimal TransactionCosts { get; set; }
         public bool CreateCashTransaction { get; set; }
 
-        public AquisitionViewModel(Aquisition aquisition)
-            : base(aquisition)
+        public AquisitionViewModel(Aquisition aquisition, StockService stockService)
+            : base(aquisition, stockService)
         {
             Units = aquisition.Units;
             AveragePrice = aquisition.AveragePrice;
             TransactionCosts = aquisition.TransactionCosts;
             CreateCashTransaction = aquisition.CreateCashTransaction;
+        }
+    }
+
+    class CashTransactionViewModel : TransactionViewModel
+    {
+        public CashTransactionViewModel(CashTransaction cashTransaction, StockService stockService)
+            : base(cashTransaction, stockService)
+        {
+
+        }
+    }
+
+    class CostBaseAdjustmentViewModel : TransactionViewModel
+    {
+        public CostBaseAdjustmentViewModel(CostBaseAdjustment costBaseAdjustment, StockService stockService)
+            : base(costBaseAdjustment, stockService)
+        {
+
+        }
+    }
+
+    class DisposalViewModel : TransactionViewModel
+    {
+        public DisposalViewModel(Disposal disposal, StockService stockService)
+            : base(disposal, stockService)
+        {
+
+        }
+    }
+
+    class IncomeReceivedViewModel : TransactionViewModel
+    {
+        public IncomeReceivedViewModel(IncomeReceived incomeReceived, StockService stockService)
+            : base(incomeReceived, stockService)
+        {
+
+        }
+    }
+
+    class OpeningBalanceViewModel : TransactionViewModel
+    {
+        public OpeningBalanceViewModel(OpeningBalance openingBalance, StockService stockService)
+            : base(openingBalance, stockService)
+        {
+
+        }
+    }
+
+    class ReturnOfCapitalViewModel : TransactionViewModel
+    {
+        public ReturnOfCapitalViewModel(ReturnOfCapital returnOfCapital, StockService stockService)
+            : base(returnOfCapital, stockService)
+        {
+
+        }
+    }
+
+    class UnitCountAdjustmentViewModel : TransactionViewModel
+    {
+        public UnitCountAdjustmentViewModel(UnitCountAdjustment unitCostAdjustment, StockService stockService)
+            : base(unitCostAdjustment, stockService)
+        {
+
         }
     }
 }
