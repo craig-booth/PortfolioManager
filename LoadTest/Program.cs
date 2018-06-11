0e10e0fae869d50e22854c8dbb0f3e7400321f85
Craig Booth <craigb@neller.com.au>
2018-06-11 09:55:46 +0930

    Upgrade to .netcore 2.1 Use builtin BackgroundService

---
+++ LoadTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+
+
+namespace LoadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var requestCount = 100;
+
+            var stockTest = new StockTest("http://localhost", new Guid("B34A4C8B-6B17-4E25-A3CC-2E512D5F1B3D"));
+
+            var stopWatch = Stopwatch.StartNew();
+            stopWatch.Start();
+            for (var i = 0; i < requestCount; i++)
+            {
+                var sendTask = stockTest.SendRequest();
+                Task.WaitAll(sendTask);
+            }
+            stopWatch.Stop();
+
+            Console.WriteLine("{0} requests in {1} millisecodes", requestCount, stopWatch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+
+    class StockTest
+    {
+        private HttpClient _HttpClient;
+
+        public StockTest(string server, Guid apiKey)
+        {
+            _HttpClient = new HttpClient();
+            _HttpClient.BaseAddress = new Uri(server);
+            _HttpClient.DefaultRequestHeaders.Add("Api-Key", apiKey.ToString());
+            _HttpClient.DefaultRequestHeaders.Accept.Clear();
+            _HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<bool> SendRequest()
+        {
+            HttpResponseMessage response = await _HttpClient.GetAsync("/api/stock/stocks");
+            return response.IsSuccessStatusCode;
+        }
+
+    }
+}
